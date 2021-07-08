1a1846de5747a09d4b6d16446d8aff8409435fea
Alex Navarro <wosorio@hightech-corp.com>
2021-07-07 20:03:01 -0600

    applying validation into reactive form with formBuilder

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-contact-form',
-  templateUrl: './contact-form.component.html',
-  styleUrls: ['./contact-form.component.scss']
+	selector: 'app-contact-form',
+	templateUrl: './contact-form.component.html',
+	styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent implements OnInit {
 
-  constructor() { }
+	public contactForm!: FormGroup;
+	
+	constructor(private formBuilder: FormBuilder) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.initForm();
+	}
 
+	public initForm(): void {
+		this.contactForm = this.formBuilder.group({
+			name: [null, [Validators.required, Validators.minLength(8)]],
+			email: [null, [Validators.required, Validators.email]],
+			subject: [null, [Validators.required, Validators.minLength(15)]],
+			comment: [null, [Validators.required, Validators.minLength(30)]]
+		});
+	}
+
+	public onSubmit(): void {
+
+		const { valid } = this.contactForm;
+
+		if (!valid) return;
+
+		alert(JSON.stringify(this.contactForm.getRawValue(), null, 4))
+	}
 }
