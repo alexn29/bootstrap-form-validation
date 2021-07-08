bff6f3a1673a7d1ffbe7752dd178c3f616c79490
Alex Navarro <wosorio@hightech-corp.com>
2021-07-07 19:27:44 -0600

    importing ReactiveFormsModule and NgBootstrapFormValidationModule. Also added a provider to use custom error messages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ContactFormComponent } from './components/contact-form/contact-form.component';
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgBootstrapFormValidationModule, CUSTOM_ERROR_MESSAGES } from 'ng-bootstrap-form-validation';
+import { CUSTOM_ERRORS } from './config/custom-errors';
 
 @NgModule({
   declarations: [
@@ -12,9 +16,18 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule,
+    NgBootstrapFormValidationModule.forRoot(),
+    NgBootstrapFormValidationModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: CUSTOM_ERROR_MESSAGES,
+      useValue: CUSTOM_ERRORS,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
